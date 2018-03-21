c8c69bbef139b59681d5ba8bb1de36bb7b7e130f
vasanwalker <36722736+vasanwalker@users.noreply.github.com>
2018-03-21 16:29:38 +0530

    Create sortarrayofn.py

---
+++ player/sortarrayofn.py
@@ -0,0 +1,17 @@
+#include<stdio.h>
+voidmain()
+int arr[10],1,n;
+print("\n enter N elements");
+scanf("%d",&n);
+for(i=;i<n;i++)
+print("enter arr[%d]=".i);
+scanf("%d",&arr[i];
+}
+for(i=0;i<n;i++)
+{
+printf("%d\n",arr[i]);
+
+
+
+}
+}
