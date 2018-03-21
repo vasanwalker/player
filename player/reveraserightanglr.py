dca2151838c99d64b00fa889e59d4a4430bd4f86
vasanwalker <36722736+vasanwalker@users.noreply.github.com>
2018-03-21 16:11:42 +0530

    Create reveraserightanglr.py

---
+++ player/reveraserightanglr.py
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("Enter the sizee");
+    scanf("%d",&n);
+    for(i=n;i>0;i--)
+    {
+        for(j=i;j>0;j--)
+        {
+            printf("1");
+            
+        }
+        printf("\n");
+    }
+}
