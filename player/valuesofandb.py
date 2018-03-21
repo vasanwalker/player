7de0a3c1921a2b85a9d86d913ae4d0a93c6fd301
vasanwalker <36722736+vasanwalker@users.noreply.github.com>
2018-03-21 16:38:29 +0530

    Create valuesofandb.py

---
+++ player/valuesofandb.py
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,d,A,B,answer,x,X;
+  cin>>A ;
+  cin>>B;
+  cin>>X;
+  a=A;
+  b=B;
+  x=X;
+  answer=a*x+b;
+  printf("answer=%d",answer);
+  
+  
+  
+  
+  
+}
