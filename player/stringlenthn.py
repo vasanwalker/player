dac4053688ac873f278de566abf93e1ac065783c
vasanwalker <36722736+vasanwalker@users.noreply.github.com>
2018-03-21 16:35:00 +0530

    Create stringlenthn.py

---
+++ player/stringlenthn.py
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+int string[array];
+printf("enter the string");
+scanf("%d" , & array of string);
+for(i=0;i<10;i++);
+{
+count i++;
+}
+printf("string");
+}
+getch()
+}
