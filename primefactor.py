f4a69c81366845d3f596cf732c2da7709a6f72e1
vasanwalker <36722736+vasanwalker@users.noreply.github.com>
2018-08-23 14:42:21 +0530

    Create primefactor.py

---
+++ primefactor.py
@@ -0,0 +1,17 @@
+def main():
+    n=int(input())
+    i=1
+    while(i<=n):
+        k=0
+        if(n%i==0):
+            j=1
+            while(j<=i):
+                if(i%j==0):
+                    k=k+1
+                j=j+1
+            if(k==2):
+                print(i)
+        i=i+1
+if __name__ == '__main__':
+
+    main()
